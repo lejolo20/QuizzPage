71b799d3f9328fed72f73927e6d9e3ce11c6c3dd
leonardo lopez <lejolo20@gmail.com>
2023-02-14 22:58:21 -0400

    finish welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import WelcomePage from './Components/WelcomePage'
 
 function App() {
   
 
   return (
+    <div>
+    <img className='blobs-blue'  src='../src/Images/blobs-blue.png'/>
+    <img className='blobs-yellow'  src='../src/Images/blobs-yellow.png'/>
     <div className="App">
-      Hello
+      <WelcomePage />
+    </div>
     </div>
   )
 }
---
+++ src/Components/WelcomePage.jsx
@@ -0,0 +1,15 @@
+import './WelcomePage.css'
+
+
+export default function WelcomePage() {
+    return(
+        <div className="welcome-page">
+           <h1>Quizzical</h1>
+           <h3>Lets prove your knowledge</h3>
+
+           <button>Start Quiz</button>
+            
+    
+        </div>
+    )
+}
