407bf8a72ec11a6279034aafa96dae56c0bdacb3
leonardo lopez <lejolo20@gmail.com>
2023-03-04 14:34:11 -0400

    page finished

--- src/Components/AnswerSection.jsx
+++ src/Components/AnswerSection.jsx
@@ -1,18 +1,57 @@
 import './QuestionSection.css'
-
+import { useState } from 'react';
 
 export default function AnswerSection(props) {
-
-    //console.log(props)
-
+    
+    const [answerSelected, setAnswerSelected] = useState(false);
+    
     const styles = {
-        backgroundColor: props.answerIsSelected ? "#94D7A2" : ""
+        backgroundColor: 
+        props.answerIsSelected 
+          ? (props.showAnswersState
+              ? (props.answerCorrect === props.answerShowed
+                ? "#94D7A2"
+                : "#F8BCBC")
+              : "#D6DBF5")
+          : "",
+
+      color: props.showAnswersState 
+      ?  (props.answerIsSelected
+          ? 'black' 
+          : '#D6DBF5')
+      : 'black',
+
+      border: props.showAnswersState 
+             ?  (props.answerIsSelected
+                 ? '1px solid black' 
+                 : '1px solid #D6DBF5')
+             : '1px solid black',
+              
+    };
+    
+    if (props.showAnswersState && !answerSelected) {
+      if (props.answerCorrect === props.answerShowed) {
+        styles.backgroundColor = "#94D7A2";
+        styles.color='#2F3137'
+      } else if (props.answerIsSelected) {
+        styles.backgroundColor = "#F8BCBC";
+      }
+    } 
+
+    function handleAnswerClick() {
+        setAnswerSelected(true);
+        props.holdAnswer();
     }
-
+          
     return (
-            
         <div className="answers" key={props.answerShowed} >
-            <button style={styles} onClick={props.holdAnswer}>{props.answerShowed}</button>   
-        </div>  )
-
-} 
+            <button 
+                style={styles}
+                disabled={props.showAnswersState} 
+                onClick={handleAnswerClick}
+            >
+                {atob(props.answerShowed)}
+            </button>   
+        </div>
+    );
+}
--- src/Components/QuestionSection.jsx
+++ src/Components/QuestionSection.jsx
@@ -2,66 +2,124 @@ import  {useEffect, useState}  from 'react'
 import './QuestionSection.css'
 import AnswerSection from './AnswerSection'
 import { v4 } from 'uuid'; 
+import Confetti from 'react-confetti'
 
-export default function QuestionSection() {
-    
+
+export default function QuestionSection(props) {
+  
+    //console.log(props)
+
     const [dataSection, setDataSection] = useState([])
-    //const [dataSectionUpdate, setDataSectionUpdate] = useState(dataSectionUpdateFunc())
+    const [correctSelection,setCorrectSelection] = useState(0)
+    const [showAnswers, setShowAnswers] = useState(false)
+    const [loading, setLoading] = useState(true)
+    const [newGame, setNewGame] = useState(0)
+            
+    const shuffledArray = (array) => array.sort((a, b) => 0.5 - Math.random())
 
-        useEffect(()=>{
-            fetch('https://opentdb.com/api.php?amount=4&type=multiple')
-            .then(res => res.json())
-            .then(data => setDataSection(data.results.map(item=>{
-                let answersArray=item.incorrect_answers.concat(item.correct_answer)
-                let newAnswers=[]
-                for(let i=0;i<4;i++){
-                    newAnswers.push(
-                        {
-                            answer:answersArray[i],
-                            isSelected:false,
-                            id:v4()
-                        })
-                }
-                 return({
-                    ...item,
-                    allAnswers: newAnswers,
-                }) 
-            })))
-        },[])
+    let urlAnyCategory = ''
+    if(props.category=='0'){
+      urlAnyCategory = `https://opentdb.com/api.php?amount=${props.howManyQuestions}&type=multiple&encode=base64`
+    } else {
+      urlAnyCategory = `https://opentdb.com/api.php?amount=${props.howManyQuestions}&category=${props.category}&type=multiple&encode=base64`
+    }
+
+    const begin = () => useEffect(()=>{
+        setLoading(true)
+        fetch(urlAnyCategory)
+        .then(res => res.json())
+        .then(data => setDataSection(data.results.map(item=>{
+            let answersArray=shuffledArray(item.incorrect_answers.concat(item.correct_answer))
+            let newAnswers=[]
+            for(let i=0;i<4;i++){
+              newAnswers.push({
+                  answer:answersArray[i],
+                  isSelected:false,
+                  id:v4()
+              })
+            }
+              return({
+                ...item,
+                allAnswers: newAnswers,
+              }) 
+          })))
+          .finally(()=>setLoading(false))
+      },[newGame])
+
+      begin()
 
 //console.log(dataSection)
 
-    function holdAnswer(id){
-        setDataSection(oldValue=>{
-            return(
-                oldValue.map(item=>{
-                    const updatedAnswers= item.allAnswers.map(element=>{
-                        return (
-                            element.id===id ? 
-                            {...element,isSelected:!element.isSelected} : 
-                            {...element,isSelected:false})
-                    })
-                    return({...item,allAnswers: updatedAnswers})
-                })  
-            )
-        })
-    } 
- 
-    const mappingQuest = dataSection.map(item=>{    
-        //console.log(item.allAnswers.map(element=>element.answer))
+  function holdAnswer(questionIndex, id) {
+    //console.log(questionIndex, id)
+    setDataSection(oldValue =>
+      oldValue.map((item, index) => {
+        if (index !== questionIndex) {
+          return item;
+        }
+        const updatedAnswers = item.allAnswers.map((element) => {
+          const isSelected = element.id === id;
+          return { ...element, isSelected };
+        });
+  
+        return { ...item, allAnswers: updatedAnswers };
+      })
+    );
+  }
+
+  function correctAnswer(){
+    let result = 0
+    let answerSelection = ''
+    dataSection.map(item=>{
+      item.allAnswers.map(element=>{
+        if (element.isSelected) return answerSelection = element.answer
+      })
+      if(item.correct_answer===answerSelection) return result=result+1
+    })
+    setCorrectSelection(result)
+  }
+
+  function showAnswersFunc(){
+    setShowAnswers(true)
+  }
+
+  function handleCorrectAndShowAnswer(){
+    if(!showAnswers){
+      showAnswersFunc()
+      correctAnswer()
+    } else {
+      setShowAnswers(false)
+      setNewGame(oldValue=>oldValue+1)
+    }
+  }
+
+   function finishConfetti(){
+    //console.log(correctSelection)
+    //console.log(props.howManyQuestions)
+    if (correctSelection==props.howManyQuestions){
+      return <Confetti />
+    }
+  } 
+
+
+    const mappingQuest = dataSection.map((item,index)=>{    
         //console.log(item.question)
+        //console.log(item.correct_answer)
         return(
             <div className="question" key={item.question}>
-                {item.question}   
+                {atob(item.question)}   
                 <div className='answer-container'>
                 {item.allAnswers.map(element=>{
+                  
                     return(
+                        
                             <AnswerSection 
                             key={element.id}
                             answerShowed={element.answer}
                             answerIsSelected={element.isSelected}
-                            holdAnswer={()=>holdAnswer(element.id)}
-                            />
+                            holdAnswer={() => holdAnswer(index, element.id)} 
+                            answerCorrect={item.correct_answer}
+                            showAnswersState={showAnswers}/>
                             )
                         })
                     }
@@ -71,9 +129,21 @@ export default function QuestionSection() {
     })
 
     return(
+      <div className='question-main'>
+        {showAnswers && finishConfetti()}
+        {loading && <div className="loading"><span>Loading...</span></div>}
         <div className='q-a-container'>
-            {mappingQuest}
-            <button className='btn-check'>Check Answers</button>
+            {mappingQuest} 
         </div>
+        <div className='result'>
+            {showAnswers && <div className='response'>You scored {correctSelection}/{props.howManyQuestions} correct answers </div>}
+            <button className='btn-check' onClick={handleCorrectAndShowAnswer}>
+              {showAnswers ? 
+              'Play Again' : 
+              'Check Answers'}
+            </button>
+           {showAnswers && <button className='btn-check' onClick={()=>window.location.reload()}>Start Again</button>}
+        </div>
+      </div>
     )
 }
